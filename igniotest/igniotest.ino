27e5c1551bce1720581b920dcd7438837af74605
Kulanjith <39695567+Kulanjith@users.noreply.github.com>
2019-08-25 22:21:21 +0530

    ignio epoch generator

--- igniotest/igniotest.ino
+++ igniotest/igniotest.ino
@@ -1,10 +1,19 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
-
+#include <NTPClient.h>
+#include <WiFiUdp.h>
 #include <ArduinoJson.h>
+
+
+
+WiFiUDP ntpUDP;
+
+const long utcOffsetInSeconds = 19800;
+NTPClient timeClient(ntpUDP,"pool.ntp.org",utcOffsetInSeconds);
 
 void setup() {
 Serial.begin(115200); //Serial connection
+timeClient.begin();
 WiFi.begin("SLT-4G-7A56", "Y61E9J076QR"); //WiFi connection
 while (WiFi.status() != WL_CONNECTED) { //Wait for the WiFI connection completion
  delay(500);
@@ -12,14 +21,21 @@ while (WiFi.status() != WL_CONNECTED) { //Wait for the WiFI connection completio
  }
 }
 void loop() {
+
+  
 if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
 DynamicJsonBuffer JSONbuffer; //Declaring static JSON buffer
 JsonObject& JSONencoder = JSONbuffer.createObject();
 JSONencoder["deviceId"] = "NODEIGNIOF101";
 
+timeClient.update();
+long unsigned int current = timeClient.getEpochTime();
+Serial.println(current);
+
+
 JsonArray& sensorData = JSONencoder.createNestedArray("sensorData");
 //JSON array
-    for(int i=0; i<5; i++){
+    //for(int i=0; i<5; i++){
 
 
         JsonObject& object = sensorData.createNestedObject();
@@ -27,8 +43,8 @@ JsonArray& sensorData = JSONencoder.createNestedArray("sensorData");
            object["co_percentage"] =  10.34 ;
            object["lp_gas_percentage"] =  29.24 ;
            object["particle_percentage"] =  12.34 ;
-           object["epoch"] =  "328102338956" ;
-      }
+           object["epoch"] =  current ;
+     // }
 
 
 
@@ -38,10 +54,10 @@ JsonArray& sensorData = JSONencoder.createNestedArray("sensorData");
 JSONencoder["epoch"] = 328102338964;
 char JSONmessageBuffer[1200];
 JSONencoder.prettyPrintTo(JSONmessageBuffer, sizeof(JSONmessageBuffer));
-Serial.println(JSONmessageBuffer);
+//Serial.println(JSONmessageBuffer);
 HTTPClient http; //Declare object of class HTTPClient
 
-http.begin("http://192.168.1.100:8082/api/device/data/"); //Specify request destination
+http.begin("http://192.168.1.103:8082/api/device/data/"); //Specify request destination
 http.addHeader("Content-Type", "application/json"); //Specify content-type header
 http.POST(JSONmessageBuffer); //Send the request
 String payload = http.getString(); //Get the response payload
